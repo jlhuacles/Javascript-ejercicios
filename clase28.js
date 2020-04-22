cfb7b38f8b3742fbbed5ba4b982d31e0553d50af
José Luis Huacles unocc <joseluishuaclesunocc@gmail.com>
2020-04-22 16:40:49 -0500

    Actualizado lo de Js hasta la ultima clase

--- clase28.js
+++ clase28.js
@@ -21,17 +21,36 @@ function onError(id){
     console.log(`Sucedió un error al obtener un personaje ${id}`);
 }
 
-obtenerPersonaje(1)
-    .then(personaje1 =>{
-        console.log(`El personaje 1 es ${personaje1.name}`);
-        return obtenerPersonaje(2);
-    })
-    .then(personaje2 =>{
-        console.log(`El personaje 2 es ${personaje2.name}`);
-        return obtenerPersonaje(3);
-    })
-    .then(personaje3 =>{
-        console.log(`El personaje 3 es ${personaje3.name}`);
-        return obtenerPersonaje(4);
-    })
-    .catch(onError)
+
+async function obtenerPersonajes(){
+    var ids = [1,2,3,4,5,6,7];
+
+    var promesas = ids.map(id=>  obtenerPersonaje(id))
+    
+    try{
+        var personajes = await Promise.all(promesas);
+        console.log(personajes);
+    }catch(id){
+       onError(id); 
+    }
+
+    
+}
+
+obtenerPersonajes();
+
+
+// /*obtenerPersonaje(1)
+//     .then(personaje1 =>{
+//         console.log(`El personaje 1 es ${personaje1.name}`);
+//         return obtenerPersonaje(2);
+//     })
+//     .then(personaje2 =>{
+//         console.log(`El personaje 2 es ${personaje2.name}`);
+//         return obtenerPersonaje(3);
+//     })
+//     .then(personaje3 =>{
+//         console.log(`El personaje 3 es ${personaje3.name}`);
+//         return obtenerPersonaje(4);
+//     })
+//     .catch(onError)*/
